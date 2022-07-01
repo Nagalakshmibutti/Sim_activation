c9ee09f180a93f175cea5750ffbabe471cf6e59c
Nagalakshmibutti <60178328+Nagalakshmibutti@users.noreply.github.com>
2022-07-01 15:30:57 +0530

    Add files via upload

---
+++ capstone_sim_activation/SIM_Activation/src/test/java/com/simactivation/SimActivationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simactivation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimActivationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
